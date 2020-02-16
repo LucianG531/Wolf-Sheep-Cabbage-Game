205644d936956f78a95dd200c12365c4fe636147
LucianG531 <luciangutica531@gmail.com>
2020-02-16 01:25:38 +0100

    Separated the objects into brand new class, added interaction between boat and other entities

--- WolfSheepCabbage.py
+++ WolfSheepCabbage.py
@@ -1,4 +1,7 @@
 import pygame
+from Entities import Entity
+
+
 
 pygame.init()
 
@@ -16,27 +19,30 @@ pygame.display.set_caption("Wolf Sheep Cabbage Game")
 game_clock = pygame.time.Clock()
 
 background = pygame.image.load("Imgs/Background.jpeg").convert()
-boat = pygame.image.load("Imgs/Boat.png")
-wolf = pygame.image.load("Imgs/Wolf.png")
+boat = Entity(screen, 270, 300 ,"Imgs/Boat.png")
+wolf = Entity(screen, 90, 350, "Imgs/Wolf.png")
 sheep = pygame.image.load("Imgs/Sheep.png")
 cabbage = pygame.image.load("Imgs/Cabbage.png")
 
 
+wolf.set_side("RIGHT")
+
 game_crash = False
 
-
-boat_x = 500
-boat_y = 300
 mov_boat = 0
 
 wolf_x = 90
 wolf_y = 350
 
+
+
+wolf_flag = 0
+
 def draw_object(obj,x,y):
     screen.blit(obj,(x,y))
 
 while not game_crash:
-    click_wolf = False
+
     screen.fill((0,0,0))
 
     screen.blit(background, (0,0))   
@@ -48,32 +54,58 @@ while not game_crash:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                mov_boat = -5
+                boat.set_mov(-5)
+                if wolf.get_pos():
+                    wolf.set_mov(-5)
             elif event.key == pygame.K_RIGHT:
-                mov_boat = 5
+                boat.set_mov(5)
+                if wolf.get_pos():
+                    wolf.set_mov(5)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                mov_boat = 0
+                boat.set_mov(0)
+                if wolf.get_pos():
+                    wolf.set_mov(0)
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             click_x, click_y = pygame.mouse.get_pos()
-            if wolf.get_rect(topleft=(wolf_x,wolf_y)).collidepoint(click_x,click_y):
-                if(boat_x == 290):
-                    
-           
+            if wolf.isClickedOn(click_x,click_y):
+                if(boat.get_x() == 270):
+                    wolf.enter_boat()
+                    wolf.set_x(boat.get_x()+50)
+                    wolf.set_y(boat.get_y()-25)
+                   
+                                           
+                   
         #    elif sheep.get_rect(topleft=(sheep_x,sheep_y)).collidepoint(click_x,click_y):
+    
          #       print("Clicked on sheep.")
           #     elif cabbage.get_rect(topleft=(cabbage_x,cabbage_y)).collidepoint(click_x, click_y):
         
-    boat_x += mov_boat
+    boat.move()
+    if wolf.get_pos():
+        wolf.move()
     
-    if boat_x <=290:
-        boat_x = 290
-    elif boat_x >= 750:
-        boat_x = 750
+    if boat.get_x() <=270:
+        boat.set_x(270)
+        boat.set_direction("LEFT")
+        if wolf.get_side == "LEFT":
+            wolf.set_side("RIGHT")
+            wolf.leave_boat()
+            wolf.set_x(90)
+            wolf.set_y(350)
+
+    elif boat.get_x() >= 750:
+        boat.set_x(750)
+        if wolf.get_side == "RIGHT":
+            wolf.set_side("LEFT")
+            wolf.leave_boat()
+            wolf.set_x(1227)
+            wolf.set_y(350)
     
-    draw_object(boat,boat_x,boat_y)
-    draw_object(wolf,wolf_x,wolf_y)
+
+    boat.draw()
+    wolf.draw()
     draw_object(sheep, 90, 450)
     draw_object(cabbage, 90, 550)
     pygame.display.update()
