94725f6d63eba789f9b3a1c61588cc825f680199
LucianG531 <luciangutica531@gmail.com>
2020-02-21 20:39:03 +0100

    Finished implementing most of game functionalities, added comments to clarify code (hopefully)

--- Entities.py
+++ Entities.py
@@ -7,6 +7,8 @@ Class that defines most properties of an entity in our game
 """
 import pygame
 
+# Entity class which represents all objects the player can interact with
+
 class Entity:
     
     def __init__(self,name,screen,pos_x, pos_y, img):
@@ -15,26 +17,42 @@ class Entity:
         self.x = pos_x
         self.y = pos_y
         self.image = pygame.image.load(img)
-        self.rect = self.image.get_rect(topleft=(self.x,self.y))
+        self.rectangle = self.image.get_rect(topleft=(self.x,self.y))
         self.mov = 0
         self.direction = "RIGHT"
         self.side = ""
-        self.left_x = 0
+       
+       # Default entity positions when they are either on the right or the left side of the screen
         self.right_x = 0
-
+        self.right_y = 0
+        self.left_x = pos_x
+        self.left_y = pos_y
     
-    def set_rect(self):
-        self.rect = self.image.get_rect(topleft = (self.x,self.y))
+    # Change the coordinates of the rectangle corresponding to the entity's image
+
+    def set_rectangle(self, x, y):
+        self.rectangle.x = x
+        self.rectangle.y = y
+
+    # Returns this entity's rectangle
+
+    def get_rectangle(self):
+        return self.rectangle
+    
+    # Sets this entity's speed parameter
 
     def set_mov(self,x):
         self.mov = x
+    
+    # Draws the entity to the screen, facing a certain direction
 
     def draw(self):
         if(self.direction) == "RIGHT":
             self.screen.blit(self.image,(self.x, self.y))
         if(self.direction) == "LEFT":
             self.screen.blit(pygame.transform.flip(self.image,True,False),(self.x,self.y))
-
+    
+    # Various getters and setters
     def set_x(self,x):
         self.x = x
     def set_y(self,y):
@@ -47,33 +65,47 @@ class Entity:
         self.direction = d
     def get_mov(self):
         return self.mov
-    
     def set_side(self,x):
         self.side = x
     def get_side(self):
         return self.side
-    
+
+    # Moves the entity to the right or left, depending on its speed parameter
+
     def move(self):
         if(self.mov >=0):
             self.direction = "RIGHT"
         else:
             self.direction = "LEFT"
         self.x = self.x + self.mov
+        self.rectangle.x = self.rectangle.x+self.mov 
     
+    # Help function, checks if the point given by x and y is inside this entity's rectangle
+    # Is used in the game loop to check if the user has clicked on this entity
+
     def isClickedOn(self, x, y):
-        return self.rect.collidepoint(x,y)
+        return self.rectangle.collidepoint(x,y)
     
+    # Checks if the entities are the same (in our case the name will be unique)
+
     def __eq__(self,other):
         if not isinstance(other, Entity):
             return NotImplemented
         return self.name == other.name
     
-    def set_left(self, x,y):
-        self.left_x = x
-        self.left_y = y
+    # Getters and setters for default left/right position of the entity
+
+    def set_right(self, x,y):
+        self.right_x = x
+        self.right_y = y
+    
+    def get_right(self):
+        return (self.right_x, self.right_y)
     
     def get_left(self):
-        return (self.left_x, self.left_y)
+        return (self.left_x, self.right_y)
+
+# Class boat, which is a special type of entity that can contain other entities and move them accross the screen with itself
 
 class Boat(Entity):
    
@@ -81,18 +113,29 @@ class Boat(Entity):
     def __init__(self, name, screen, pos_x, pos_y,  img):
        Entity.__init__(self, name,screen, pos_x, pos_y, img)
        self.contains = None
-    
+       
+   # Overloads the move method from Entity, as boat does not have a rectangle
+
+    def move(self):
+        if(self.mov >=0):
+            self.direction = "RIGHT"
+        else:
+            self.direction = "LEFT"
+        self.x = self.x + self.mov
+
+    # Puts the Entity obj inside of the boat
+
     def enter_boat(self, obj):
         self.contains = obj
+
+    # Kicks all entities out of the boat
 
     def leave_boat(self):
         self.contains = None
     
+    # Returns the entity that is inside of the boat at the moment
+
     def get_content(self):
         return self.contains
+    
 
-    def content(self, obj):
-        if self.contains == obj:
-            return True
-        else:
-            return False
--- WolfSheepCabbage.py
+++ WolfSheepCabbage.py
@@ -5,8 +5,12 @@ from Entities import Entity, Boat
 pygame.init()
 
 
+# Setting background size equal to background image size
+
 screen_w = 1317
 screen_h = 656
+
+# Creating the game window
 
 screen = pygame.display.set_mode((screen_w, screen_h))
 pygame.display.set_caption("Wolf Sheep Cabbage Game")
@@ -15,25 +19,43 @@ pygame.display.set_caption("Wolf Sheep Cabbage Game")
 #Boat size: 290 150
 
 
+# Initializing the game clock
 game_clock = pygame.time.Clock()
+
+
+# Initializing the game font
 game_font = pygame.font.Font("Imgs/nimrod_mt.ttf", 48)
 
 
-
+# Loading the images for the background and all entities  the initial game state
 background = pygame.image.load("Imgs/Background.jpeg").convert()
 boat = Boat("boat",screen, 270, 300 ,"Imgs/Boat.png")
 wolf = Entity("wolf",screen, 90, 350, "Imgs/Wolf.png")
 sheep = Entity("sheep", screen, 90, 450, "Imgs/Sheep.png")
 cabbage = Entity("cabbage", screen, 90, 550, "Imgs/Cabbage.png")
 
+
+# Adding all animals to a list to easly operate on all at the same time when needed
+
 animals = [wolf, sheep, cabbage]
 
+
 for animal in animals:
+
+    # All animals are initially on the left side
+
     animal.set_side("LEFT")
-    animal.set_left(screen_w - 250, animal.get_y())
+    
+    # Setting where the animal is placed once moved to the right side
+
+    animal.set_right(screen_w - 250, animal.get_y())
+
+
+# Variable to check if game is still running
 
 game_crash = False
 
+# Method to define behaviour in case a game loss condition
 
 def game_over():
     text = game_font.render("GAME OVER", True, (0,0,0))
@@ -42,27 +64,45 @@ def game_over():
         animal.set_x(2000)
         animal.set_y(2000)
 
-def draw_object(obj,x,y):
-    screen.blit(obj,(x,y))
+
+def game_won():
+    text = game_font.render("YOU WON!", True, (0,0,0))
+    screen.blit(text, (520,280))
+    for animal in animals:
+        animal.set_x(2000)
+        animal.set_y(2000)
+
+# Game loop
 
 while not game_crash:
+    
+    # Drawing background
 
     screen.fill((0,0,0))
 
     screen.blit(background, (0,0))   
     
-    
+    # Defining behaviour for different events
+
     for event in pygame.event.get():
+       
         if event.type == pygame.QUIT:
             game_crash = True
         
+        # If user presses the left key, the boat's and boat content's speed parameter is set to -5 (So boat moves left)
+        # If user presses the right key, the boat's and boat content's speed parameter is set to 5 (So boat moves right)
+        # If user lets go of either key, the boat's and boat content's speed parameter is set to 0 (So the boat stops moving)
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 boat.set_mov(-5)
+               
                 if not boat.get_content() == None:        
                     boat.get_content().set_mov(-5)
+            
             elif event.key == pygame.K_RIGHT:
                 boat.set_mov(5)
+              
                 if not boat.get_content() == None:
                     boat.get_content().set_mov(5)
                 
@@ -71,17 +111,40 @@ while not game_crash:
                 boat.set_mov(0)
                 if not boat.get_content() == None:   
                     boat.get_content().set_mov(0)
+        
+        # User can click on an animal
+        # If animal is clicked on and the boat has no content, and the boat and animal are on the same side, 
+        #then the animal enters the boat
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             click_x, click_y = pygame.mouse.get_pos()
             for animal in animals:
                 if animal.isClickedOn(click_x,click_y):
-                    if (boat.get_x() == 270) and (boat.get_content() == None):
-                        boat.enter_boat(animal)
-                        animal.set_x(boat.get_x()+50)
-                        animal.set_y(boat.get_y()-25)
-                     
-                              
+                    if (boat.get_x() == 270 or boat.get_x() == 750) and animal.get_side() == boat.get_side():
+                        if boat.get_content() == None:
+
+                            boat.enter_boat(animal)
+                            animal.set_x(boat.get_x()+50)
+                            animal.set_y(boat.get_y()-25)
+                            animal.set_rectangle(boat.get_x()+50, boat.get_y()-25)
+                            animal.set_direction(boat.get_side())
+
+                        elif boat.get_content() == animal:
+                            if boat.get_side() == "LEFT":
+                                x,y = animal.get_left()
+                                orientation = "LEFT" 
+                            elif boat.get_side() == "RIGHT":
+                                x,y = animal.get_right()
+                                orientation = "RIGHT"
+                            
+                            animal.set_x(x)
+                            animal.set_y(y)
+                            animal.set_direction(orientation)
+                            animal.set_rectangle(x,y)
+                            boat.leave_boat()
+
+    # Boat side determined by which half of the screen it finds itself in
+    # This is done to give the player some leeway in case they make a mistake and want to correct it
 
     if boat.get_x() <= 510:
         boat.set_side("LEFT")
@@ -91,38 +154,51 @@ while not game_crash:
         boat.set_side("RIGHT")
 
                    
-       
+    # Move the boat and its contents
+
     boat.move()
     if not boat.get_content() == None:
         boat.get_content().move()
-        boat.get_content().set_rect()
         boat.get_content().set_side(boat.get_side())
+    
+    # Prevents boat from leaving the boundaries of the water on the screen
+
     if boat.get_x() <=270:
         boat.set_x(270)
         boat.set_direction("LEFT")
-        
-       
+        if not boat.get_content() == None:
+            boat.get_content().set_x(boat.get_x()+50)
+            boat.get_content().set_rectangle(boat.get_x()+50, boat.get_y()-25)
     elif boat.get_x() >= 750:
         boat.set_x(750)
-        
         if not boat.get_content() == None:
-            x,y = boat.get_content().get_left()
-            boat.get_content().set_x(x)
-            boat.get_content().set_y(y)
-            boat.get_content().set_direction("LEFT")
-            boat.leave_boat()
+            boat.get_content().set_x(boat.get_x()+50)        
+            boat.get_content().set_rectangle(boat.get_x()+50, boat.get_y()-25)
+ 
+    # Game conditions are as follows:
+    # Wolf and sheep cannot be on the same side if the boat is not there
+    # Sheep and cabbage cannot be on the same side if the boat is not there
 
-            
     if wolf.get_side() == sheep.get_side() and not wolf.get_side() == boat.get_side():
         game_over()
         
     if sheep.get_side() == cabbage.get_side() and not sheep.get_side() == boat.get_side():
         game_over()
-        
+   
+   # TODO: Dont end game before player clicked last animal and put it into right place
+
+    if  wolf.get_side() == sheep.get_side() and sheep.get_side() == cabbage.get_side() and cabbage.get_side() == "RIGHT" and boat.get_x() == 750:
+        game_won()
+
+
+    # Draws all entities to the screen
+
     boat.draw()
     wolf.draw()
     sheep.draw()
     cabbage.draw()
+    
+    # Updates the game screen and sets the game clock to 60
     pygame.display.update()
     game_clock.tick(60)
 
